c00184de31d799d5d6bab14d1527527ec68e2b58
Sam Bauers <sam@wopr.com.au>
2022-06-18 16:38:59 +1000

    chore(deps): remove chalk dependency

--- src/lib/program/chalk.ts
+++ src/lib/program/chalk.ts
@@ -1,10 +1,23 @@
-import chalk from 'chalk'
+const wrap = (code: string) => `\u001B[${code}m`
+
+const style = (
+  input: string,
+  ...styles: Array<[number | string, number | string]>
+) => {
+  let output = input
+  styles.forEach(([start, end]) => {
+    output = `${wrap(String(start))}${output}${wrap(String(end))}`
+  })
+  return `${wrap('0')}${output}${wrap('0')}`
+}
+
+const underline = (input: string) => style(input, [4, 24])
 
 export default {
-  heading: chalk.underline,
-  dim: chalk.dim,
-  error: chalk.rgb(255, 255, 255).bgRedBright,
-  warning: chalk.rgb(255, 255, 255).bgRgb(255, 100, 0),
-  success: chalk.rgb(255, 255, 255).bgGreen,
-  emphasise: chalk.underline,
+  heading: underline,
+  dim: (input: string) => style(input, [2, 22]),
+  error: (input: string) => style(input, ['38;5;231', 39], [101, 49]),
+  warning: (input: string) => style(input, ['38;5;231', 39], ['48;5;208', 49]),
+  success: (input: string) => style(input, ['38;5;231', 39], [42, 49]),
+  emphasise: underline,
 }
