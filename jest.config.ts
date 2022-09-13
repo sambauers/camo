70b574fcb924c36f01d15d89d1a099018694acdb
Sam Bauers <sam@wopr.com.au>
2022-09-13 18:29:23 +1000

    chore(deps): minor updates, config, bump v1.1.9
    * ts-jest automatically detects ESM
    * Downgrade node types to match version number

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,6 @@ import { InitialOptionsTsJest } from 'ts-jest'
 
 const config: InitialOptionsTsJest = {
   preset: 'ts-jest',
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
