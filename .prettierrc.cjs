2d0b43e1a3e2fa714d0b87f4f60fd678e69b43dd
Sam Bauers <sam@wopr.com.au>
2023-07-23 16:04:56 +1000

    chore(deps): update packages, avoid ts-node esm issue

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,12 @@
+// @ts-check
+
 /** @type {import('prettier').Options} */
 const config = {
-  trailingComma: 'es5',
+  printWidth: 80,
   semi: false,
   singleQuote: true,
   jsxSingleQuote: true,
+  trailingComma: 'es5',
 }
 
 module.exports = config
